ea551a0ccab66608f54097889e7c1d94ca916396
MarkKoz <KozlovMark@gmail.com>
2017-12-06 14:19:47 -0800

    Implemented the menu and main loop.

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <utility>
 
 #include "Tree.hpp"
 
-int main() {
+/**
+ * Represents a menu selection.
+ */
+enum Selection {
+    ADD = 1,
+    SHOW = 2,
+    EXIT = 3
+};
 
+/**
+ * Displays a menu with selectable options to the user and prompts the user to
+ * make a @c Selection.
+ *
+ * After the selected operation completes, the user is displayed the menu and
+ * is prompted for a selection again until @c Selection::EXIT is selected.
+ *
+ * If the selection is not in the range [1,3], the user is prompted to make a
+ * new selection.
+ *
+ * @return          The @c Selection made.
+ */
+Selection menu();
+
+int main() {
+    // Tree tree;
+    Selection selection;
+
+    do {
+        selection = menu();
+
+        switch (selection) {
+            case Selection::ADD:
+                // tree.add();
+                break;
+            case Selection::SHOW:
+                // tree.show();
+                break;
+            default:
+                break;
+        }
+
+        std::cout << '\n';
+    } while (selection != Selection::EXIT);
+}
+
+Selection menu() {
+    std::cout << "     Menu\n";
+    std::cout << "==============\n";
+    std::cout << "1. Add Student\n";
+    std::cout << "2. Show List\n";
+    std::cout << "3. Exit\n\n";
+    std::cout << "Enter a selection: ";
+
+    std::string selection;
+
+    while (true) {
+        std::getline(std::cin, selection);
+
+        try {
+            const int i = std::stoi(selection);
+
+            if (i < 1 || i > 4) {
+                throw std::invalid_argument("The selection is out of the valid "
+                                            "range [1,3].");
+            }
+
+            return static_cast<Selection>(i);
+        } catch (std::invalid_argument& e) {
+            std::cout << "Invalid selection, try again: ";
+        }
+    }
 }
