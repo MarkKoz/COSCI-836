1409acf40f5041e5a17158b5abf43e95c5464514
MarkKoz <KozlovMark@gmail.com>
2017-12-09 12:56:04 -0800

    Made Tree threaded and modified node insertion helper to support that.

--- include/Tree.hpp
+++ include/Tree.hpp
@@ -22,6 +22,7 @@ class Node {
 public:
     Node<T>* left = nullptr;
     Node<T>* right = nullptr;
+    bool threaded = false;
 
     explicit Node(const T& value) : data(value) { }
 
@@ -33,6 +34,11 @@ public:
 
     const T* value() const {
         return std::addressof(data);
+    }
+
+    void setThread(Node<T>* thread) {
+        left = thread;
+        threaded = true;
     }
 
 private:
@@ -103,17 +109,11 @@ public:
 
         if (empty()) {
             root = node;
+
+            return true;
         } else {
-            Node<T>** pos = getPos(root, node->value());
-
-            if (!pos) { // nullptr means value is a duplicate.
-                return false;
-            }
-
-            *pos = node;
+            return insertNode(root, node);
         }
-
-        return true;
     }
 
     /**
@@ -143,34 +143,51 @@ private:
     }
 
     /**
-     * @brief Finds the position at which a new @c Node should be inserted using
-     * recursion.
+     * @brief Recursively finds the position at which @c node can be inserted
+     * and then inserts @c node at that position.
      *
      * Searches for an empty child @c Node (@c left or @c right) depending on
-     * the evaluation of the comparison between @c value and the @c parent's
-     * @c value. If the @c Node is not empty, the function is recursively called
-     * starting at that occupied child @c Node.
+     * the evaluation of the comparison between @c node's @c value and @c
+     * parent's @c value. If the @c Node is occupied, the function is
+     * recursively called starting at that occupied child @c Node.
+     *
+     * Once an empty position is found, @c node is inserted into that position.
      *
      * @param   parent  The @c Node at which to begin the search.
-     * @param   value   The value of the @c Node to be inserted.
-     * @return          A pointer to an empty child @c Node pointer or @c
-     *                  nullptr if @c value already exists in the container.
+     * @param   node    The @c Node to be inserted.
+     * @return          @c true if the insertion was successful; @c false
+     *                  otherwise.
      */
-    Node<T>** getPos(Node<T>* parent, const T* value) {
-        if (*parent->value() > *value) { // Goes left.
-            if (parent->left) { // Left node exists.
-                return getPos(parent->left, value);
-            } else { // Left node doesn't exist.
-                return std::addressof(parent->left);
+    bool insertNode(Node<T>* parent, Node<T>* node) {
+        if (*parent->value() > *node->value()) { // Goes left.
+            if (parent->left) { // Left node exists & is occupied.
+                return insertNode(parent->left, node);
+            } else { // Left node doesn't exist; found the free position.
+                // The succeeding node for in-order traversal is the parent
+                // of the left node.
+                node->setThread(parent);
+                parent->left = node; // Insert the node.
+
+                return true;
             }
-        } else if (*parent->value() < *value) { // Goes right.
-            if (parent->right) { // Right node exists.
-                return getPos(parent->right, value);
-            } else { // Right node doesn't exist.
-                return std::addressof(parent->right);
+        } else if (*parent->value() < *node->value()) { // Goes right.
+            // Checks if the right node exists i.e. it isn't null and it isn't
+            // threaded.
+            if (parent->right && !parent->threaded) {
+                return insertNode(parent->right, node);
+            } else {
+                // The right node doesn't exist. Either its parent is the last
+                // node or it is threaded.
+
+                // Transfer parent's thread to the new node.
+                node->setThread(parent->right);
+                parent->threaded = false; // Parent isn't threaded anymore.
+                parent->right = node; // Insert the node.
+
+                return true;
             }
         } else { // Value is equal to the parent's value; no duplicates allowed.
-            return nullptr;
+            return false;
         }
     }
 };
